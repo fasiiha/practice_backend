f212e75966d99602a38f1c3c55a98dac48e72917
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-02-10 18:03:46 +0500

    config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-connectDB();
 dotenv.config({
   path: "./env",
 });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Connected to port " + process.env.PORT);
+    });
+  })
+  .catch(function (err) {
+    console.error(" Couldn't connect to database" + err.message);
+  });
